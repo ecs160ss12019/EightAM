364dce656619eca2d3918b02c2dc2cda445d03d6
mesagoh <mesagoh@gmail.com>
2019-07-10 12:16:34 -0700

    progress

--- Asteroids/app/src/main/java/EightAM/asteroids/GameObject.java
+++ Asteroids/app/src/main/java/EightAM/asteroids/GameObject.java
@@ -1,6 +1,49 @@
 package EightAM.asteroids;
 
-interface GameObject {
+import android.graphics.RectF;
+
+abstract class GameObject {
+    protected float velX;
+    protected float velY;
+    protected float posX;
+    protected float posY;
+    protected float delX;
+    protected float delY;
+
+    private RectF hitbox;   // manipulate shape using set()
+
+    protected void spawn(float x, float y) {
+        this.posX = x;
+        this.posY = y;
+    }
+
+    /**
+     * does literally nothing
+     * @param x some integer idk
+     * @return another integer lol
+     */
+    protected int foo(int x) {return 0;}
+
+    protected void draw() {
+        /*
+        Use canvas and paint to draw objects
+        */
+    }
+
+    protected void move(/*velocity*/) {
+        /*
+        Takes in velocity, computes new position
+        Set new position.
+         */
+    }
+
+    protected void collision() {
+        /*
+        Possibly take in 2 RectF objects
+        Check intersection of hitboxes
+         */
+    }
+
 
 
 }

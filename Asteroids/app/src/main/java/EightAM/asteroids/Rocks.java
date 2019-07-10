032aae2635a93c8fef7ec6158286631c8f264d95
Kenny Li <kennyli@campus-021-075.ucdavis.edu>
2019-07-10 12:27:46 -0700

    update

--- Asteroids/app/src/main/java/EightAM/asteroids/Rocks.java
+++ Asteroids/app/src/main/java/EightAM/asteroids/Rocks.java
@@ -1,4 +1,40 @@
 package EightAM.asteroids;
 
+
 class Rocks {
+
+    /*
+    Goals:
+    - Spawn on space outskirts
+    - rocks generates rocks in a collision
+    - rocks float in space
+    - rocks collide with everting other object except itself
+    - speed: constant and dependent on size
+
+    Interact with:
+    Space
+    GameObject
+     */
+
+    public int DIM_SMALL;
+    public int DIM_MEDIUM;
+    public int DIM_LARGE;
+
+    //int velocity;
+    int dimentsion;
+
+    public Rocks() {
+        //dimension
+        //velocity
+        //
+
+
+    }
+
+    public void spawn() {
+
+    }
+
+    public
+
 }

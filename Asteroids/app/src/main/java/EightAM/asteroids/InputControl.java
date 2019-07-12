e849a777242752c98a2790b61a281e7125a14a44
Adrian Wang <atwang9503@github.com>
2019-07-12 10:06:05 -0700

    Changed to fullscreen layout Created temporary buttons Linked InputControl to buttons Began implementation of GameView Raised java level
    Signed-off-by: Adrian Wang <atwang@ucdavis.edu>

--- Asteroids/app/src/main/java/EightAM/asteroids/InputControl.java
+++ Asteroids/app/src/main/java/EightAM/asteroids/InputControl.java
@@ -1,4 +1,80 @@
 package EightAM.asteroids;
 
+import android.app.Activity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
 class InputControl {
+    static class Control {
+        boolean UP = false;
+        boolean DOWN = false;
+        boolean RIGHT = false;
+        boolean LEFT = false;
+        boolean SPECIAL_1 = false;
+    }
+
+    static class ButtonSet {
+        Button left;
+        Button right;
+        Button up;
+        Button down;
+        Button shoot;
+    }
+
+    static Control playerInput = new Control();
+    static ButtonSet buttonSet = new ButtonSet();
+
+    // Connects buttons
+    static void initializeButtons(Activity activity) {
+        buttonSet.left = activity.findViewById(R.id.left_button);
+        buttonSet.right = activity.findViewById(R.id.right_button);
+        buttonSet.up = activity.findViewById(R.id.up_button);
+        buttonSet.down = activity.findViewById(R.id.down_button);
+        buttonSet.shoot = activity.findViewById(R.id.shoot_button);
+        buttonSet.left.setOnTouchListener((View view, MotionEvent event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                Log.d("debug", "left_down");
+                playerInput.LEFT = true;
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                Log.d("debug", "left_up");
+                playerInput.LEFT = false;
+            }
+            return true;
+        });
+        buttonSet.right.setOnTouchListener((View view, MotionEvent event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                playerInput.RIGHT = true;
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                playerInput.RIGHT = false;
+            }
+            return true;
+        });
+        buttonSet.up.setOnTouchListener((View view, MotionEvent event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                playerInput.UP = true;
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                playerInput.UP = false;
+            }
+            return true;
+        });
+        buttonSet.down.setOnTouchListener((View view, MotionEvent event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                playerInput.DOWN = true;
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                playerInput.DOWN = false;
+            }
+            return true;
+        });
+        buttonSet.shoot.setOnTouchListener((View view, MotionEvent event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                playerInput.SPECIAL_1 = true;
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                playerInput.SPECIAL_1 = false;
+            }
+            return true;
+        });
+    }
+
 }
--- Asteroids/app/src/main/java/EightAM/asteroids/MainActivity.java
+++ Asteroids/app/src/main/java/EightAM/asteroids/MainActivity.java
@@ -2,13 +2,24 @@ package EightAM.asteroids;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
-class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Hide the status bar.
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
         setContentView(R.layout.activity_main);
+        InputControl.initializeButtons(this);
     }
 }

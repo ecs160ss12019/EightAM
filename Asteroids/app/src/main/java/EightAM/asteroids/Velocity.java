dfa0098b45250cd03ff42dda542992bc7864a3fb
Irene <ireneho.imyh@gmail.com>
2019-07-15 13:59:15 -0700

    began velocity class

---
+++ Asteroids/app/src/main/java/EightAM/asteroids/Velocity.java
@@ -0,0 +1,18 @@
+package EightAM.asteroids;
+
+/**
+ * The Velocity class holds components of velocity (x and y components)
+ * and has a method to calculate total velocity.
+ *
+ * Its intention is to simplify the calculation of total velocity
+ * for instances of GameObject.
+ */
+public class Velocity {
+    float velX;
+    float vely;
+    float vel;
+
+    protected Velocity() {
+
+    }
+}

fd5023be74e6a7116330e417bd20b02569f7c325
Irene <ireneho.imyh@gmail.com>
2019-07-15 14:06:47 -0700

    added documentation

--- Asteroids/app/src/main/java/EightAM/asteroids/Velocity.java
+++ Asteroids/app/src/main/java/EightAM/asteroids/Velocity.java
@@ -10,16 +10,24 @@ package EightAM.asteroids;
 public class Velocity {
     float velX;
     float velY;
-    float vel;
 
+    /**
+     * Creates an instance of Velocity.
+     * Meant to be used within a GameObject.
+     * @param x component of velocity
+     * @param y component of velocity
+     */
     protected Velocity(float x, float y) {
         velX = x;
         velY = y;
-        vel = (float)Math.sqrt(Math.pow(this.velX, 2) + Math.pow(this.velY, 2));
     }
 
+    /**
+     * Calculates the total velocity of the object with which this instance
+     * of velocity is associated.
+     * @return total velocity of the object
+     */
     protected float totalVelocity() {
-        vel = (float)Math.sqrt(Math.pow(this.velX, 2) + Math.pow(this.velY, 2));
-        return vel;
+        return (float)Math.sqrt(Math.pow(this.velX, 2) + Math.pow(this.velY, 2));
     }
 }

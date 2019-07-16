aa60a31f9b6c56b17bad89fa55c7a16894b18b1e
Irene <ireneho.imyh@gmail.com>
2019-07-16 10:39:54 -0700

    set size and ID

--- Asteroids/app/src/main/java/EightAM/asteroids/Alien.java
+++ Asteroids/app/src/main/java/EightAM/asteroids/Alien.java
@@ -1,4 +1,17 @@
 package EightAM.asteroids;
 
-class Alien {
+class Alien extends GameObject {
+    // ---------------Member variables-------------
+    enum Size { SMALL, LARGE }; // denotes what kind of alien
+    private Size size;
+    // ---------------Member methods --------------
+
+    protected Alien(Size s) {
+        size = s;
+
+        // might use later
+        this.objectID = ObjectID.ALIEN;
+    }
+
+
 }

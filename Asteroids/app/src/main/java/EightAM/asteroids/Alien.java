4e9a42cb08bfcdc9cfe85d9727e3e55a8f081095
Irene <ireneho.imyh@gmail.com>
2019-07-16 11:00:41 -0700

    took out unused import statement

--- Asteroids/app/src/main/java/EightAM/asteroids/Alien.java
+++ Asteroids/app/src/main/java/EightAM/asteroids/Alien.java
@@ -2,7 +2,6 @@ package EightAM.asteroids;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.RectF;
 import java.util.Random;
 

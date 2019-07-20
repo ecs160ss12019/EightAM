c47c6dcfc591df5856a47aa4c09016e103ff810e
TimothyV97 <timothyvan97@gmail.com>
2019-07-20 11:59:01 -0700

    Adding functionality to Factories: create, destroy and respawn

--- Asteroids/app/src/main/java/EightAM/asteroids/AsteroidFactory.java
+++ Asteroids/app/src/main/java/EightAM/asteroids/AsteroidFactory.java
@@ -1,4 +1,33 @@
 package EightAM.asteroids;
 
+
+
 public class AsteroidFactory implements Factory {
+    int spaceWidth;
+    int spaceHeight;
+    int numOfAsteroids;
+
+    AsteroidFactory(int screenWidth, int screenHeight, int initialAsteroids) {
+        spaceHeight = screenHeight;
+        spaceWidth = screenWidth;
+        numOfAsteroids = initialAsteroids;
+    }
+    
+    GameObject create(Context context, GameObject object) {
+        return (new Asteroid(this, spaceWidth, spaceHeight, shipPosX, shipPosY, context));
+    }
+
+    void createBelt(Context context, ArrayList <GameObject> objects){
+        for (int i = 0; i < numOfAsteroids; i++) {
+            asteroidBelt.add(new Asteroid(this, spaceWidth, spaceHeight, shipPosX, shipPosY, context));
+        }
+    }
+
+    void destroy(GameObject object){
+
+    }
+
+    void respawn(GameObject object){
+
+    }
 }

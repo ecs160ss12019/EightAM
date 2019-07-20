6b3db840c225c5e40b8642a44fb1dc85175bb2dd
TimothyV97 <timothyvan97@gmail.com>
2019-07-20 13:07:39 -0700

    Changed Factory to abstract class, trimmed AsteroidFactory

--- Asteroids/app/src/main/java/EightAM/asteroids/AsteroidFactory.java
+++ Asteroids/app/src/main/java/EightAM/asteroids/AsteroidFactory.java
@@ -3,32 +3,14 @@ package EightAM.asteroids;
 import android.content.Context;
 import java.util.ArrayList;
 
-public class AsteroidFactory implements Factory {
-    int spaceWidth;
-    int spaceHeight;
-    int numOfAsteroids;
+public class AsteroidFactory extends Factory {
 
-    public AsteroidFactory(int screenWidth, int screenHeight, int initialAsteroids) {
-        spaceHeight = screenHeight;
-        spaceWidth = screenWidth;
-        numOfAsteroids = initialAsteroids;
+    public AsteroidFactory(GameModel gameModel) {
+        model = gameModel;
+        objectsToDelete = new ArrayDeque<Integer>();
     }
     
     public GameObject create(Context context, GameObject object) {
-        return (new Asteroid(this, spaceWidth, spaceHeight, shipPosX, shipPosY, context));
-    }
-
-    public void createBelt(Context context, ArrayList<GameObject> objects){
-        for (int i = 0; i < numOfAsteroids; i++) {
-            objects.add(new Asteroid(this, spaceWidth, spaceHeight, shipPosX, shipPosY, context));
-        }
-    }
-
-    public void destroy(GameObject object){
-
-    }
-
-    public void respawn(GameObject object){
-
+        return (new Asteroid(model, model.spaceWidth, model.spaceHeight, model.shipPosX, model.shipPosY, context));
     }
 }

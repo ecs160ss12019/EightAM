1ae48cfb69ff8d6f2361fce2f1f962b1a6b56ee5
Irene <ireneho.imyh@gmail.com>
2019-07-27 21:38:24 -0700

    added documentation

--- Asteroids/app/src/main/java/EightAM/asteroids/BulletGenerator.java
+++ Asteroids/app/src/main/java/EightAM/asteroids/BulletGenerator.java
@@ -16,6 +16,11 @@ public class BulletGenerator extends GameObjectGenerator {
         return instance;
     }
 
+    /**
+     * Makes a new basic bullet and puts it into objectMap.
+     * @param objectMap the map of object IDs and object instances from GameModel
+     * @param shooter the one who is shooting the bullet
+     */
     public void createBullet(Map<ObjectID, GameObject> objectMap, Shooter shooter) {
         BasicBulletSpec spec = prepareSpec(shooter);
 
@@ -23,6 +28,11 @@ public class BulletGenerator extends GameObjectGenerator {
         objectMap.put(bullet.getID(), bullet);
     }
 
+    /**
+     * Modifies the pos and orientation inside a spec.
+     * @param shooter the one shooting the bullet
+     * @return a basic bullet spec
+     */
     private BasicBulletSpec prepareSpec(Shooter shooter) {
         // prepare the bullet spec
         BasicBulletSpec spec = new BasicBulletSpec();

4e0b67dc57142640e4b2d4ed7df6cf90f8087756
mesagoh <mesagoh@gmail.com>
2019-07-28 21:23:46 -0700

    modified particle spec to be more consistent

--- Asteroids/app/src/main/java/EightAM/asteroids/specs/BaseParticleSpec.java
+++ Asteroids/app/src/main/java/EightAM/asteroids/specs/BaseParticleSpec.java
@@ -2,29 +2,17 @@ package EightAM.asteroids.specs;
 
 import android.graphics.Point;
 
-public class BaseParticleSpec extends BaseSpec {
-    public static String tag = "particle";
-    public static String bitMapName = null;
-    public static int resID = 0;
-    public static Point dimensions = new Point(5,5);
-    public static float dbmRatio = 0;
-    public static String paintName = "particle";
-    public static int speed = 10;
-    public static int maxDistance = 5;
-    public static int duration = 10;
+public abstract class BaseParticleSpec extends BaseSpec {
+    public static int speed;
+    public static int duration;
 
-    public BaseParticleSpec() {
+    public BaseParticleSpec(String tag, String bitMapName, int resID, Point dimensions, float dbmRatio, String paintName) {
         super(tag, bitMapName, resID, dimensions, dbmRatio, paintName);
-        setSpeed(speed);
-        setMaxDistance(maxDistance);
-        setDuration(duration);
     }
 
     void setSpeed(int speed) {
         this.speed = speed;
     }
-
-    void setMaxDistance(int maxDistance) { this.maxDistance = maxDistance; }
 
     void setDuration(int duration) { this.duration = duration; }
 }
--- Asteroids/app/src/main/java/EightAM/asteroids/specs/ParticleSpec.java
+++ Asteroids/app/src/main/java/EightAM/asteroids/specs/ParticleSpec.java
@@ -2,29 +2,20 @@ package EightAM.asteroids.specs;
 
 import android.graphics.Point;
 
-public class ParticleSpec extends BaseSpec {
+public class ParticleSpec extends BaseParticleSpec {
     public static String tag = "particle";
     public static String bitMapName = null;
     public static int resID = 0;
-    public static Point dimensions = new Point(5,5);
-    public static float dbmRatio = 0;
+    public static Point dimensions = new Point(10,10);
+    public static float dbmRatio = 3f;
     public static String paintName = "particle";
     public static int speed = 10;
-    public static int maxDistance = 5;
-    public static int duration = 10;
+    public static int duration = 1000;
 
     public ParticleSpec() {
         super(tag, bitMapName, resID, dimensions, dbmRatio, paintName);
         setSpeed(speed);
-        setMaxDistance(maxDistance);
         setDuration(duration);
     }
 
-    void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    void setMaxDistance(int maxDistance) { this.maxDistance = maxDistance; }
-
-    void setDuration(int duration) { this.duration = duration; }
 }
